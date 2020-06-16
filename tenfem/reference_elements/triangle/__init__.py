976d1d6f5cb74851031303177acbe518753a4147
danieltait <tait.djk@gmail.com>
2020-06-16 15:15:02 +0100

    adding TriangleElement to __init__.py

--- tenfem/reference_elements/triangle/__init__.py
+++ tenfem/reference_elements/triangle/__init__.py
@@ -12,8 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-
-
-class TriangleElement(object):
-    """ Reference element for a triangle mesh. """
-    pass
+from .triangle_element import TriangleElement
