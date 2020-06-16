d938fd3f92c58271ab62193eab357ed7071b7614
danieltait <tait.djk@gmail.com>
2020-06-16 14:09:07 +0100

    basemesh.py --> base_mesh.py

--- tenfem/mesh/__init__.py
+++ tenfem/mesh/__init__.py
@@ -12,5 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-from .basemesh import BaseMesh
+from .base_mesh import BaseMesh
 
