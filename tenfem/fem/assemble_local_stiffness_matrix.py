854837f76173891a27ff70b0395652cc8ec378fa
danieltait <tait.djk@gmail.com>
2020-06-16 20:27:47 +0100

    function to assemble the local stiffness matrix

--- tenfem/fem/assemble_local_stiffness_matrix.py
+++ tenfem/fem/assemble_local_stiffness_matrix.py
@@ -13,17 +13,41 @@
 # limitations under the License.
 # ============================================================================
 """ Assembly of the local elements of the stiffness matrix. """
+import tensorflow as tf
+import tenfem
 
 
-def assemble_local_stiffness_matrix(diffusion_coefficient, mesh, element):
-    """
+def assemble_local_stiffness_matrix(scalar_diffusion_coefficient: tf.Tensor,
+                                    mesh: tenfem.mesh.BaseMesh,
+                                    element) -> tf.Tensor:
+    """ Function to assemble the local stiffness matrix over a mesh.
+
+    ToDo(dan): Implement tensor valued diffusion coefficients.
 
     Args:
-        diffusion_coefficient:
-        mesh:
-        element:
+        scalar_diffusion_coefficient: A float `Tensor` of shape
+          `[..., n_elements, element_dim] giving the values of the scalar
+            diffusion coefficient.
+        mesh: A `tenfem.mesh.BaseMesh` object representing the finite element
+          mesh of the domain.
+        element: A `tenfem.reference_element` object describing the elements of the mesh.
 
     Returns:
-
+        local_stiffness_matrix: A `Tensor` of shape `[..., n_elements, element_dim, element_dim]`
+          giving the local values of the stiffness matrix tensor over elements.
     """
-    pass
+    element_nodes = tf.gather(mesh.nodes, mesh.elements)
+
+    wts, quad_nodes = element.get_quadrature_nodes_and_weights(element.get_quadrature_order())
+    _, pf_shape_fn_grad, jac_det = element.isomap(element_nodes, quad_nodes)
+
+    wxarea = jac_det * wts / 2
+    ak_q = tf.reduce_sum(pf_shape_fn_grad[..., tf.newaxis] * pf_shape_fn_grad[..., tf.newaxis, :], axis=0)
+
+    # ak_q.shape = [n_elements, len(wts), element_dim, element_dim]
+
+    # scalar diff coeff should be shape mesh.get_quadrature_nodes()[..., 0] = [n_elements, len(wts)]
+    ak_q = ak_q * scalar_diffusion_coefficient[..., tf.newaxis, tf.newaxis]
+
+    return tf.reduce_sum(ak_q * wxarea[..., tf.newaxis, tf.newaxis], axis=-3)
+
