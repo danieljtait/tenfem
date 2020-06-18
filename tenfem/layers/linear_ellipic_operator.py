dfb10b1b6b5cb7e39fa941d8dc698f596e588784
danieltait <tait.djk@gmail.com>
2020-06-18 22:56:03 +0100

    basic structure and call method for LinearEllipticOperator

--- tenfem/layers/linear_ellipic_operator.py
+++ tenfem/layers/linear_ellipic_operator.py
@@ -14,7 +14,8 @@
 # ============================================================================
 """ Layer to form and solve linear elliptic operators. """
 from typing import Callable, Union
-
+import tensorflow as tf
+import tenfem
 from tenfem.layers import BaseFEMLayer
 
 
@@ -24,10 +25,69 @@ class LinearEllipticOperator(BaseFEMLayer):
                  diffusion_coefficient: Callable,
                  source: Callable,
                  transport_vector_field: Union[Callable, None] = None,
-                 name='linear_elliptic_opeartor',
+                 name: str ='linear_elliptic_opeartor',
+                 boundary_condition: str = 'dirichlet',
                  *args, **kwargs):
         super(LinearEllipticOperator, self).__init__(name=name, *args, **kwargs)
 
         self._diffusion_coefficient = diffusion_coefficient
         self._source = source
         self._transport_vector_field = transport_vector_field
+        self._boundary_condition = boundary_condition
+
+        if self._boundary_condition == 'dirichlet':
+            try:
+                self._boundary_values = kwargs['boundary_values']
+            except KeyError:
+                raise ValueError(''.join((
+                    'If this model uses Dirichlet boundary conditions then ',
+                    'the values of the solution on the boundary must be supplied',
+                    'or else a callable to evaluate these solutions')))
+        else:
+            raise NotImplementedError('Only Dirichlet boundary conditions currently implemented')
+
+
+    def call(self, mesh_tensor_repr):
+        mesh = tenfem.mesh.utils.mesh_from_tensor_repr(mesh_tensor_repr,
+                                                       self.reference_element)
+        element = self.reference_element
+
+        # shape [mesh.n_elements, element_dim, spatial_dim]
+        mesh_quadrature_nodes = element.get_quadrature_nodes(mesh)
+
+        element_dim = tf.shape(mesh_quadrature_nodes)[-2]
+        spatial_dim = tf.shape(mesh.nodes)[-1]
+
+        # evaluate the diffusion coefficient at the quadrature nodes
+        flat_mesh_quadrature_nodes = tf.reshape(mesh_quadrature_nodes, [-1, spatial_dim])
+        diffusion_coeff_vals = tf.reshape(
+            self.diffusion_coefficient(flat_mesh_quadrature_nodes),
+            [-1, mesh.n_elements, element_dim])
+
+        local_stiffness_mat = tenfem.fem.assemble_local_stiffness_matrix(
+            diffusion_coeff_vals, mesh, element)
+        local_stiffness_mat = tf.reshape(local_stiffness_mat,
+                                         [-1, mesh.n_elements, element_dim, element_dim])
+
+        batch_size = tf.shape(local_stiffness_mat)[0]
+        elements = tf.tile(mesh.elements[tf.newaxis, ...], [batch_size, 1, 1])
+
+        global_stiffness_mat = tenfem.fem.scatter_matrix_to_global(
+            local_stiffness_mat, elements, mesh.n_nodes)
+
+        source_vals = tf.reshape(
+            self.source(flat_mesh_quadrature_nodes),
+            [-1, mesh.n_elements, element_dim])
+
+        local_load_vector = tenfem.fem.assemble_local_load_vector(
+            source_vals, mesh, element)
+        local_load_vector = tf.reshape(local_load_vector,
+                                       [-1, mesh.n_elements, element_dim])
+
+        batch_size = tf.shape(local_load_vector)[0]
+        elements = tf.tile(mesh.elements[tf.newaxis, ...], [batch_size, 1, 1])
+
+        load_vector = tenfem.fem.scatter_vector_to_global(
+            local_load_vector, elements, mesh.n_nodes)
+
+        u =
