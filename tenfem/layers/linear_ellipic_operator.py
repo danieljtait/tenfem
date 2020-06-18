475c2821d6a318e4fa5c40d03382bf914c0cfd79
danieltait <tait.djk@gmail.com>
2020-06-18 22:50:02 +0100

    basic __init__ of LinearEllitpicOperator

--- tenfem/layers/linear_ellipic_operator.py
+++ tenfem/layers/linear_ellipic_operator.py
@@ -13,6 +13,21 @@
 # limitations under the License.
 # ============================================================================
 """ Layer to form and solve linear elliptic operators. """
+from typing import Callable, Union
+
+from tenfem.layers import BaseFEMLayer
 
 
+class LinearEllipticOperator(BaseFEMLayer):
+    """ tf.keras Layer for solving linear elliptic second order PDES. """
+    def __init__(self,
+                 diffusion_coefficient: Callable,
+                 source: Callable,
+                 transport_vector_field: Union[Callable, None] = None,
+                 name='linear_elliptic_opeartor',
+                 *args, **kwargs):
+        super(LinearEllipticOperator, self).__init__(name=name, *args, **kwargs)
 
+        self._diffusion_coefficient = diffusion_coefficient
+        self._source = source
+        self._transport_vector_field = transport_vector_field
