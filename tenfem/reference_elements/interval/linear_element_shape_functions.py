19ea53fbe77d9159eb11d461b7b847381d0235c4
danieltait <tait.djk@gmail.com>
2020-06-19 18:47:08 +0100

    basic p1 shape function for interval element

--- tenfem/reference_elements/interval/linear_element_shape_functions.py
+++ tenfem/reference_elements/interval/linear_element_shape_functions.py
@@ -13,3 +13,26 @@
 # limitations under the License.
 # ============================================================================
 """ Shape functions for linear elements. """
+import tensorflow as tf
+
+
+def p1_shape_fn(r):
+    """ Shape function of the linear Lagrange polynomial on an interval element.
+
+    Args:
+        r: A flat `Tensor` of shape `[..., n]` the canonical coordinate on the
+          reference interval element `I = [0, 1]`.
+
+    Returns:
+        shape_fn_vals: A float `Tensor` of shape `[..., n, 2] with each
+          `[..., n, i]` the evaluation of the ith shape function at coordinate
+          `r[..., n]`.
+        shape_fn_grads: A float `Tensor` giving the gradient with respect to
+          the canincal coordinate of the shape function. A float tensor of
+          shape `[1, ..., n, 2]`.
+    """
+    shape_fn_vals = tf.concat((1 - r[..., tf.newaxis], r[..., tf.newaxis]), axis=-1)
+    shape_fn_grads = tf.concat((-tf.ones_like(r)[..., tf.newaxis],
+                                tf.ones_like(r)[..., tf.newaxis]), axis=-1)[tf.newaxis, ...]
+
+    return shape_fn_vals, shape_fn_grads
