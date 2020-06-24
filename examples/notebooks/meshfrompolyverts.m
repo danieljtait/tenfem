72d6c362ec3f90991a73f9da948537ccfdbf111b
danieltait <tait.djk@gmail.com>
2020-06-24 14:17:04 +0100

    deleted matlab file

--- examples/notebooks/meshfrompolyverts.m
+++
@@ -1,7 +0,0 @@
-function [p, e, t] = meshfrompolyverts(X, hmax)
-  pg = polyshape(X(:, 1), X(:, 2))
-  tr = triangulation(pg);
-  model = createpde();
-  geom = geometryFromMesh(model, tr.Points', tr.ConnectivityList');
-  femmesh = generateMesh(model, 'Hmax', hmax, 'GeometricOrder', 'linear');
-  [p, e, t] = meshToPet(femmesh);
